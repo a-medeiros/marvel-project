3a06524879591ffbfc83d15134c32216eb44919d
Ana Lúcia Medeiros <ana.medeiros0097@gmail.com>
2022-07-13 16:05:10 -0300

    Setting environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  privateKey: '4cb94aecd28a4e71b9c3829f3376d4e99e34c9eb',
+  publicKey: '72ea2d0b3c9ccc1228d9f4da35bb8667',
 };
